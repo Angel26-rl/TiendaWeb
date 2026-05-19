2e751fe1ce6ad4ef97a4038288298f0472b5aeef
Angel Geovanni Ramos López <aramosl17@miumg.edu.gt>
2026-05-18 21:34:33 -0600

    Carrito funcional y nuevo catálogo tecnológico

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+function agregarCarrito(nombre, precio){
+
+    carrito.push({
+        nombre: nombre,
+        precio: precio
+    });
+
+    localStorage.setItem(
+        "carrito",
+        JSON.stringify(carrito)
+    );
+
+    alert(nombre + " agregado al carrito");
+
+}
+
+function mostrarCarrito(){
+
+    let contenedor =
+    document.getElementById("carrito-items");
+
+    if(!contenedor){
+        return;
+    }
+
+    let total = 0;
+
+    contenedor.innerHTML = "";
+
+    carrito.forEach(producto => {
+
+        contenedor.innerHTML += `
+            <div class="item-carrito">
+                <h3>${producto.nombre}</h3>
+                <p>Q ${producto.precio}</p>
+            </div>
+        `;
+
+        total += producto.precio;
+
+    });
+
+    document.getElementById("total")
+    .innerText = "Total: Q " + total;
+
+}
+
 function confirmarCompra(){
 
     document.getElementById("formularioCompra")
@@ -5,6 +54,8 @@ function confirmarCompra(){
 
     document.getElementById("mensaje")
     .classList.remove("oculto");
+
+    localStorage.removeItem("carrito");
 
 }
 
