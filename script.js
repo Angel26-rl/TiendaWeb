dfb8b7e0289bda118611beb84deaa8d4631955a6
Angel Geovanni Ramos López <aramosl17@miumg.edu.gt>
2026-05-29 14:26:44 -0600

    Versión finall

--- script.js
+++ script.js
@@ -131,7 +131,7 @@ function confirmarCompra(){
         <div class="factura-box">
 
             <h2>
-                VELTRIX STORE
+                TECHLIFE STORE
             </h2>
 
             <p>
